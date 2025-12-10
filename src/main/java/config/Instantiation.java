50d3d44ff1456d0b226294f655c583823ebbdd0d
Rodrigo Araujo <rasch.dev@gmail.com>
2025-12-10 12:51:38 -0300

    Created Post and PostRepository. Updated Instantiation.

--- src/main/java/config/Instantiation.java
+++ src/main/java/config/Instantiation.java
@@ -1,27 +1,42 @@
 package config;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.TimeZone;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import domain.Post;
 import domain.User;
+import repositories.PostRepository;
 import repositories.UserRepository;
 
 @Configuration
 public class Instantiation implements CommandLineRunner {
 	@Autowired UserRepository userRepo;
+	@Autowired PostRepository postRepo;
 
 	@Override
 	public void run(String... args) throws Exception {
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/mm/YYYY");
+		sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
+		
 		userRepo.deleteAll();
+		postRepo.deleteAll();
 		
 		User u1 = new User(null, "maria", "maria@maria");
 		User u2 = new User(null, "alex", "alex@alex");
 		User u3 = new User(null, "sonic", "sonic@sonic");
-		
 		userRepo.saveAll(Arrays.asList(u1,u2,u3));
+		
+		
+		
+		Post p1 = new Post(null, "Alow", "Tchau", u1);
+		Post p2 = new Post(null, "Hello", "Bye", u2);
+		Post p3 = new Post(null, "Catchau", "Catchoi", u3);
+		postRepo.saveAll(Arrays.asList(p1,p2,p3));
 		
 	}
 
---
+++ src/main/java/domain/Post.java
@@ -0,0 +1,84 @@
+package domain;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.time.Instant;
+import java.util.Date;
+import java.util.Objects;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document
+public class Post implements Serializable {
+    @Serial private static final long serialVersionUID = 1L;
+    
+	@Id private String id;
+	private String title, body;
+	private Date date;
+	private User author;
+	
+	public Post() {}
+
+	public Post(String id, String title, String body, User author) {
+		this.id = id;
+		this.title = title;
+		this.body = body;
+		this.date = Date.from(Instant.now());
+		this.author = author;
+	}
+
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public User getAuthor() {
+		return author;
+	}
+	public void setAuthor(User author) {
+		this.author = author;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getBody() {
+		return body;
+	}
+	public void setBody(String body) {
+		this.body = body;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Post other = (Post) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+	
+}
