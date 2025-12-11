548d91402598767975d23cda275dcb52308f747d
Rodrigo Araujo <rasch.dev@gmail.com>
2025-12-11 10:12:32 -0300

    Created PostService and PostService::findById

---
+++ src/main/java/services/PostService.java
@@ -0,0 +1,24 @@
+package services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import domain.Post;
+import repositories.PostRepository;
+import services.exceptions.ObjectNotFoundException;
+
+@Service
+public class PostService {
+	@Autowired private PostRepository repo;
+	
+	public Post findById(String id) {
+		Object post = repo.findById(id);
+		
+		if(post == null) {
+			throw new ObjectNotFoundException("Objeto não encontrado.");
+		}
+		else {
+			return (Post) post;
+		}
+	}
+}
